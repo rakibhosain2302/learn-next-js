0c36eeafb36682909ce740a29ebef7d9d5976209
Rakib Hosain <rakibhosain2302@gmail.com>
2026-06-27 19:27:10 +0600

    function, event, stateful components Handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
+"use client";
+import StateComponent from "./Components/state";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const handleClick = () => {
+    alert("Button clicked!");
+  };
+
   return (
-    <div>
+    <div className={styles["page-container"]}>
       <main>
+        <StateComponent />
         <h1>Welcome</h1>
-        <p>Your journey begins here.</p>
+        <p className={styles.content}>Your journey begins here.</p>
+
+        {/* Function and Event Handling */}
+         <p className={styles["sub-title"]}>Function and Event Handling</p>
+        <button onClick={handleClick}>Click me</button>
+
+        {/* Components and State Handling */}
+        <p className={styles["sub-title"]}>Stateful components manage dynamic data.</p>
+        <StateComponent/>
       </main>
     </div>
   );
