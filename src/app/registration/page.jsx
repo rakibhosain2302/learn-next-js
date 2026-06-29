61cda7067c59fb1db6e697108d7559341a6ce251
Rakib Hosain <rakibhosain2302@gmail.com>
2026-06-29 14:30:54 +0600

    Next JS middleware learn

---
+++ src/app/registration/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='page-container'>
+      <h1 className='page-header'>Register</h1>
+      <p className='content'>
+        Create a new account to get started.
+      </p>
+    </div>
+  )
+}
+
+export default page
---
+++ src/middleware.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { NextResponse } from 'next/server'
+
+const middleware = (request) => {
+  return NextResponse.redirect(new URL("/registration", request.url))
+}
+
+export default middleware
+
+export const config ={
+    matcher:"/class/:path*"
+}
